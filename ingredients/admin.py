0bf8f8ca5776199c129fdea6d4117475a5595234
DEVANSH <devanshsharma416@example.com>
2022-03-02 18:52:32 +0530

    First commit

---
+++ ingredients/admin.py
@@ -0,0 +1,8 @@
+
+# Register your models here.
+# cookbook/ingredients/admin.py
+from django.contrib import admin
+from .models import Category, Ingredient
+
+admin.site.register(Category)
+admin.site.register(Ingredient)
